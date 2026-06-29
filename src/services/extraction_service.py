80d623e3bdccb52e81673080076c603d0ba76c82
Tim 's Gravemade <tim.sgravemade@croonwolterendros.nl>
2026-06-29 15:33:39 +0200

    Made extract_relatics standalone function instead of class

--- src/services/extraction_service.py
+++ src/services/extraction_service.py
@@ -9,43 +9,38 @@ ELEMENT_OPERATION = "dip_elements"
 ELEMENT_REPORT_PART = "Elements"
 DATA_MODEL_OPERATION = "dip_data_model_2"
 
-class RelaticsExtractorService():
+def extract_relatics(
+    client_id: str,
+    client_secret: str,
+    environment: str
+): 
+    client = RelaticsClient(client_id,client_secret,environment)
+    tables = {}
     
-    def __init__(
-        self, 
-        client_id: str,
-        client_secret: str,
-        environment: str
-    ) -> None:
-        self.client = RelaticsClient(client_id,client_secret,environment)
-
-    def extract_relatics(self): 
-        tables = {}
+    for workspace_id in WORKSPACE_IDS:
+        elements_root = client.get_request(workspace_id,ELEMENT_OPERATION)
+        elements_df = parse_xml(elements_root,ELEMENT_REPORT_PART)
         
-        for workspace_id in WORKSPACE_IDS:
-            elements_root = self.client.get_request(workspace_id,ELEMENT_OPERATION)
-            elements_df = parse_xml(elements_root,ELEMENT_REPORT_PART)
+        for idx in elements_df.index:             
+            element = elements_df["Element"][idx]
+            element_id = elements_df["ElementID"][idx]
+            parameters = {
+                "ConfigurationOfRef": element_id
+            }
             
-            for idx in elements_df.index:             
-                element = elements_df["Element"][idx]
-                element_id = elements_df["ElementID"][idx]
-                parameters = {
-                    "ConfigurationOfRef": element_id
-                }
-                
-                print(f"retrieving data for {element}")
-                
-                element_root = self.client.get_request(
-                    workspace_id,
-                    DATA_MODEL_OPERATION,
-                    parameters
-                )
-                
-                extractor = RelaticsExtractor(
-                    element_root,
-                    workspace_id 
-                )
-                
-                tables.update(extractor.create_element_tables())
-                
-        return tables
+            print(f"retrieving data for {element}")
+            
+            element_root = client.get_request(
+                workspace_id,
+                DATA_MODEL_OPERATION,
+                parameters
+            )
+            
+            extractor = RelaticsExtractor(
+                element_root,
+                workspace_id 
+            )
+            
+            tables.update(extractor.create_element_tables())
+            
+    return tables
