f573c07b2cf09000541e43c79d42dc0a5ba52573
Nick van Vliet <nick.vanvliet@croonwolterendros.nl>
2026-08-07 16:14:47 +0200

    Literally one single test.

--- tests/unit/test_validator.py
+++ tests/unit/test_validator.py
@@ -88,11 +88,26 @@ def test_normalize_tables_never_add_required_column():
     If a column doesn't exist but is required, the function should never add it.
     """
     
-def test_is_valid_schema_input_valid():
+def test_is_valid_schema_input_valid(tables_fixture):
     """
     Als input volledig voeldoet -> doe niks
     """
+    test_schema = {
+        "users": {
+            "name": {"required": True},
+            "age": {"required": False, "default": None},
+            "email": {"required": True}
+        },
+        "movies": {
+            "name": {"required": True},
+            "genre": {"required": False, "default": None}
+        }
+    }
+
+    report = is_valid_schema(tables=tables_fixture, schema=test_schema)
     
+    assert report == None
+
 def test_is_valid_schema_input_missing_columns():
     """
     Als column mist bij tenminste een van de tabelen raise error.
