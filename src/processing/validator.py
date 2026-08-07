5bf69183eb9c9aae64f285d570c000fc8ae9ecf0
Tim 's Gravemade <tim.sgravemade@croonwolterendros.nl>
2026-08-07 15:31:18 +0200

    WIP

--- src/processing/validator.py
+++ src/processing/validator.py
@@ -5,17 +5,19 @@ from typing import Dict
 from src.processing.schema import SCHEMA
 
 def normalize_tables(
-    tables: Dict[str, pd.DataFrame]
-) -> pd.DataFrame:
+    tables: Dict[str, pd.DataFrame],
+    schema: Dict[str, dict] = SCHEMA
+) -> Dict[str, pd.DataFrame]:
     """ 
     Function that takes a dict of tables and adds to each 
     table the unrequired columns according to the schema, 
     if they are not present.
     """
-    pass
+    return tables
 
 def is_valid_schema(
-    tables: Dict[str, pd.DataFrame]
+    tables: Dict[str, pd.DataFrame],
+    schema: Dict[str,dict] = SCHEMA
 ) -> None:
     """
     Checks tables against schema and returns validation report.
